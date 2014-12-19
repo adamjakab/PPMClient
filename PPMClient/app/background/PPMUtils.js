43c3a766ebc1473769bb9a7e50a5196d4bef8b49
adamjakab <jakabadambalazs@gmail.com>
2014-12-19 23:31:49 +0100

    fixing getGibberish method

--- PPMClient/app/background/PPMUtils.js
+++ PPMClient/app/background/PPMUtils.js
@@ -34,6 +34,61 @@ define([
                 log("SHUTDOWN COMPLETED", "info");
                 fulfill();
             });
+        },
+
+        /**
+         * Returns a very ugly string
+         * @param {int} minLength - if not set if will default to 0
+         * @param {int} [maxLength] if not set if will default to minLength
+         * @param {boolean} [useSpecial=false]
+         * @returns {string}
+         */
+        getGibberish: function(minLength, maxLength, useSpecial) {
+            var ugly_chars = {
+                "alpha": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                "numeric": "0123456789",
+                "special": "#@?!|&%^*+-=.:,;/([{<>}])"
+            };
+            useSpecial = (useSpecial===true);
+            minLength = minLength || 0;
+            maxLength = maxLength || minLength;
+            var length = this.getRandomNumberInRange(minLength, maxLength);
+            //
+            var charTypes = ["alpha","numeric"];
+            if(useSpecial) {
+                charTypes.push("special");
+            }
+            //
+            var lengthPerType = Math.floor(length/charTypes.length);
+            var typeLength = [];
+            typeLength["alpha"] = lengthPerType;//ALPHA
+            typeLength["numeric"] = lengthPerType;//NUMERIC
+            typeLength["special"] = length-(2*lengthPerType);//SPECIAL
+
+            var answer = '';
+            var t, chars;
+            while(answer.length < length) {
+                t = charTypes[Math.floor(Math.random() * charTypes.length)];
+                typeLength[t]--;
+                if(!typeLength[t]) {
+                    charTypes.splice(charTypes.indexOf(t),1);
+                }
+                chars = ugly_chars[t];
+                answer += chars[Math.floor(chars.length*Math.random())];
+            }
+            return(answer);
+        },
+
+        /**
+         * Returns a number N where N is between min and max (inclusive, ie. N can be min or max)
+         * @param {number} min
+         * @param {number} max
+         * @returns {number}
+         */
+        getRandomNumberInRange: function(min, max) {
+            return(min + Math.round(Math.random()*(max-min)));
         }
+
+
     };
 });
--- PPMClient/tests/background/PPMUtilsSpec.js
+++ PPMClient/tests/background/PPMUtilsSpec.js
@@ -6,13 +6,43 @@ define(['config', 'TestUtils', 'PPMUtils'],
 
         describe("PPMUtils Tests", function () {
 
-            it("initialize should return Promise", function () {
+            it("should return Promise on initialize", function () {
                 TestUtils.isPromise(PPMUtils.initialize());
             });
 
-            it("shutdown should return Promise", function () {
+            it("should return Promise on shutdown", function () {
                 TestUtils.isPromise(PPMUtils.shutdown());
             });
+
+            it("should return random number in range", function () {
+                var random;
+                var min = 0; var max = 64;
+                for (var i=0; i<100; i++) {
+                    random = PPMUtils.getRandomNumberInRange(min,max);
+                    expect(random).toBeGreaterThan(min-1);
+                    expect(random).toBeLessThan(max+1);
+                }
+            });
+
+            it("should return gibberish string with exact length", function () {
+                var min = 0; var max = 64;
+                for (var i=0; i<100; i++) {
+                    var random = PPMUtils.getRandomNumberInRange(min,max);
+                    var gibberish = PPMUtils.getGibberish(random, random, true);
+                    expect(gibberish.length).toBe(random);
+                }
+            });
+
+            it("should return gibberish string with length between min and max", function () {
+                var min = 0; var max = 10;
+                for (var i=0; i<10; i++) {
+                    var gibberish = PPMUtils.getGibberish(min, max, true);
+                    expect(gibberish.length).toBeGreaterThan(min-1);
+                    expect(gibberish.length).toBeLessThan(max+1);
+                    dump(gibberish);
+                }
+            });
+
 
         });
     }
