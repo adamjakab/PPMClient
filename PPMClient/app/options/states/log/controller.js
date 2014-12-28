afc7a35a54027a4c165294bb7e6f0045c9e6241c
adamjakab <jakabadambalazs@gmail.com>
2014-12-28 22:09:35 +0100

    Options - now can see logs

--- PPMClient/app/options/states/log/controller.js
+++ PPMClient/app/options/states/log/controller.js
@@ -2,20 +2,21 @@ define([
     'angular'
 ], function () {
     angular.module('optionsApp').controller('log.controller',
-        function ($scope, settings, $state) {
+        function ($scope, settings, $state, $timeout) {
             $scope.settings = settings;
             var PPM = chrome.extension.getBackgroundPage().ParanoiaPasswordManager;
             var CHROMESTORAGE = PPM.getComponent("CHROMESTORAGE");
-            var UTILS = PPM.getComponent("UTILS");
+            var LOGGER = PPM.getComponent("LOGGER");
             /** log shorthand */
             var log = function (msg, type) {
-                PPM.getComponent("LOGGER").log(msg, "OPTIONS(log)", type);
+                LOGGER.log(msg, "OPTIONS(log)", type);
             };
 
             /**
              * Defaults
              */
             $scope.logged_in = PPM.isLoggedIn();
+            $scope.logItems = LOGGER.getLogObjects();
 
             /**
              * If user is not logged in redirect to "noprofile" state
@@ -23,6 +24,18 @@ define([
             if (!$scope.logged_in && !$state.is("noprofile")) {
                 $state.go("noprofile");
             }
+
+            /**
+             * Auto refresh
+             * todo: probably this is not needed if we set up a provider
+             */
+            var stop;
+            var autorefreshLogs = function() {
+                stop = $timeout(function() {
+                    autorefreshLogs();
+                }, 1000);
+            };
+            autorefreshLogs();
 
 
         }
